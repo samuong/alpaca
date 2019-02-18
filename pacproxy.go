59c0889641c697495fa5ec255b419174a4973331
Sam Uong <samuong@gmail.com>
2019-02-18 20:18:34 +1100

    Handle HTTP CONNECT (for HTTPS)

--- pacproxy.go
+++ pacproxy.go
@@ -1,29 +1,98 @@
 package main
 
 import (
+	"crypto/tls"
+	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
+	"sync"
 )
 
+func main() {
+	s := &http.Server{
+		// Set the addr to localhost so that we only listen locally.
+		Addr:    "localhost:3128",
+		Handler: http.HandlerFunc(handler),
+		// TODO: Implement HTTP/2 support. In the meantime, set
+		// TLSNextProto to a non-nil value to disable HTTP/2.
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler))}
+	log.Fatal(s.ListenAndServe())
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodConnect {
+		connect(w, r)
+	} else {
+		direct(w, r)
+	}
+}
+
+func connect(w http.ResponseWriter, r *http.Request) {
+	// TODO: should probably put a timeout on this
+	server, err := net.Dial("tcp", r.Host)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	h, ok := w.(http.Hijacker)
+	if !ok {
+		msg := fmt.Sprintf("Can't hijack connection to %v", r.Host)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	client, _, err := h.Hijack()
+	if err != nil {
+		// The response status has already been sent, so if hijacking
+		// fails, we can't return an error status to the client.
+		// Instead, log the error and finish up.
+		log.Printf("Error hijacking connection to %v", r.Host)
+		server.Close()
+		return
+	}
+	go func() {
+		defer server.Close()
+		defer client.Close()
+		var wg sync.WaitGroup
+		wg.Add(2)
+		go transfer(&wg, server, client)
+		go transfer(&wg, client, server)
+		wg.Wait()
+	}()
+}
+
+func transfer(wg *sync.WaitGroup, dst, src net.Conn) {
+	defer wg.Done()
+	_, err := io.Copy(dst, src)
+	if err != nil {
+		log.Printf("Error copying from %v to %v",
+			src.RemoteAddr().String(), dst.RemoteAddr().String())
+	}
+}
+
+func direct(w http.ResponseWriter, r *http.Request) {
 	resp, err := http.DefaultTransport.RoundTrip(r)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer resp.Body.Close()
+	// TODO: Don't retransmit hop-by-hop headers.
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers#hbh
 	for k, vs := range resp.Header {
 		for _, v := range vs {
 			w.Header().Add(k, v)
 		}
 	}
 	w.WriteHeader(resp.StatusCode)
-	io.Copy(w, resp.Body)
-}
-
-func main() {
-	http.HandleFunc("/", handler)
-	// bind to localhost so that we only listen locally
-	log.Fatal(http.ListenAndServe("localhost:3128", nil))
+	_, err = io.Copy(w, resp.Body)
+	if err != nil {
+		// The response status has already been sent, so if copying
+		// fails, we can't return an error status to the client.
+		// Instead, log the error.
+		log.Printf("Error copying response body from %v", r.Host)
+		return
+	}
 }
