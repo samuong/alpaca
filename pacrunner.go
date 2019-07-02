19f50c921a25c6095793e621b2805db1d01735f6
Sam Uong <samuong@gmail.com>
2019-07-02 20:33:25 +1000

    Implement the rest of the Netscape PAC functions

--- pacrunner.go
+++ pacrunner.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"encoding/binary"
 	"errors"
 	"github.com/gobwas/glob"
 	"github.com/robertkrimen/otto"
 	"io"
+	"net"
 	"net/url"
 	"strings"
 	"sync"
+	"time"
 )
 
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Proxy_servers_and_tunneling/Proxy_Auto-Configuration_(PAC)_file
@@ -26,11 +29,25 @@ func NewPACRunner(r io.Reader) (*PACRunner, error) {
 		}
 		err = vm.Set(name, handler)
 	}
-	// TODO: These three functions are the only ones that are used by the
-	// ANZ PAC file. Implement the rest later.
 	set("isPlainHostName", isPlainHostName)
 	set("dnsDomainIs", dnsDomainIs)
+	set("localHostOrDomainIs", localHostOrDomainIs)
+	set("isResolvable", isResolvable)
+	set("isInNet", isInNet)
+	set("dnsResolve", dnsResolve)
+	set("convert_addr", convertAddr)
+	set("myIpAddress", myIpAddress)
+	set("dnsDomainLevels", dnsDomainLevels)
 	set("shExpMatch", shExpMatch)
+	set("weekdayRange", func(fc otto.FunctionCall) otto.Value {
+		return weekdayRange(fc, time.Now())
+	})
+	set("dateRange", func(fc otto.FunctionCall) otto.Value {
+		return dateRange(fc, time.Now())
+	})
+	set("timeRange", func(fc otto.FunctionCall) otto.Value {
+		return timeRange(fc, time.Now())
+	})
 	if err != nil {
 		return nil, err
 	}
@@ -84,6 +101,93 @@ func dnsDomainIs(call otto.FunctionCall) otto.Value {
 	return toValue(strings.HasSuffix(host, domain))
 }
 
+func localHostOrDomainIs(call otto.FunctionCall) otto.Value {
+	host := call.Argument(0).String()
+	hostdom := call.Argument(1).String()
+	return toValue(host == hostdom || strings.HasPrefix(hostdom, host+"."))
+}
+
+func isResolvable(call otto.FunctionCall) otto.Value {
+	host := call.Argument(0).String()
+	_, err := net.LookupHost(host)
+	return toValue(err == nil)
+}
+
+func isInNet(call otto.FunctionCall) otto.Value {
+	host := call.Argument(0).String()
+	pattern := call.Argument(1).String()
+	mask := call.Argument(2).String()
+	buf := net.ParseIP(mask).To4()
+	m := net.IPv4Mask(buf[0], buf[1], buf[2], buf[3])
+	maskedIP := resolve(host).Mask(m)
+	maskedPattern := net.ParseIP(pattern).To4().Mask(m)
+	return toValue(maskedIP.Equal(maskedPattern))
+}
+
+func dnsResolve(call otto.FunctionCall) otto.Value {
+	host := call.Argument(0).String()
+	return toValue(resolve(host).String())
+}
+
+func resolve(host string) net.IP {
+	if ip := net.ParseIP(host); ip != nil {
+		// The given host is already an IP(v4) address; just return it.
+		return ip.To4()
+	}
+	addrs, err := net.LookupHost(host)
+	if err != nil {
+		return nil
+	}
+	for _, addr := range addrs {
+		// There might be multiple IP addresses for this host. Return the first IPv4 address
+		// that we can find.
+		if ipv4 := net.ParseIP(addr).To4(); ipv4 != nil {
+			return ipv4
+		}
+	}
+	return nil
+}
+
+func convertAddr(call otto.FunctionCall) otto.Value {
+	ipaddr := call.Argument(0).String()
+	ipv4 := net.ParseIP(ipaddr).To4()
+	if ipv4 == nil {
+		return toValue(0)
+	}
+	return toValue(binary.BigEndian.Uint32(ipv4))
+}
+
+func myIpAddress(call otto.FunctionCall) otto.Value {
+	// When the host has multiple IPs, Chrome seems to go to some length to find the best one
+	// (see https://cs.chromium.org/chromium/src/net/proxy_resolution/pac_library.cc?g=0&l=22),
+	// but we'll just return the first non-loopback IPv4 address that we find (or "127.0.0.1" if
+	// there are none) and hope this is good enough.
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return otto.UndefinedValue()
+	}
+	for _, addr := range addrs {
+		s := addr.String()
+		// Remove the first rune that is not either a digit or a dot, as well as anything
+		// that follows it. This turns strings like "192.0.2.1:25" and "192.168.1.6/24" into
+		// parsable IPv4 addresses.
+		if i := strings.IndexFunc(s, func(r rune) bool {
+			return !strings.ContainsRune("0123456789.", r)
+		}); i != -1 {
+			s = s[0:i]
+		}
+		if ipv4 := net.ParseIP(s).To4(); ipv4 != nil && !ipv4.IsLoopback() {
+			return toValue(ipv4.String())
+		}
+	}
+	return toValue("127.0.0.1")
+}
+
+func dnsDomainLevels(call otto.FunctionCall) otto.Value {
+	host := call.Argument(0).String()
+	return toValue(strings.Count(host, "."))
+}
+
 func shExpMatch(call otto.FunctionCall) otto.Value {
 	str := call.Argument(0).String()
 	shexp := call.Argument(1).String()
@@ -92,4 +196,146 @@ func shExpMatch(call otto.FunctionCall) otto.Value {
 		return otto.UndefinedValue()
 	}
 	return toValue(g.Match(str))
+}
+
+func weekdayRange(call otto.FunctionCall, now time.Time) otto.Value {
+	if call.Argument(len(call.ArgumentList)-1).String() == "GMT" {
+		now = now.In(time.UTC)
+	}
+	weekdays := map[string]time.Weekday{
+		"SUN": time.Sunday, "MON": time.Monday, "TUE": time.Tuesday, "WED": time.Wednesday,
+		"THU": time.Thursday, "FRI": time.Friday, "SAT": time.Saturday,
+	}
+	wd1, ok := weekdays[call.Argument(0).String()]
+	if !ok {
+		return otto.UndefinedValue()
+	}
+	wd2, ok := weekdays[call.Argument(1).String()]
+	if !ok {
+		return toValue(now.Weekday() == wd1)
+	} else if wd1 <= wd2 {
+		return toValue(wd1 <= now.Weekday() && now.Weekday() <= wd2)
+	} else {
+		return toValue(wd1 == now.Weekday() || wd2 == now.Weekday())
+	}
+}
+
+func dateRange(call otto.FunctionCall, now time.Time) otto.Value {
+	argc := len(call.ArgumentList)
+	if call.Argument(argc-1).String() == "GMT" {
+		now = now.In(time.UTC)
+		argc--
+	}
+
+	var days []int
+	var months []time.Month
+	var years []int
+
+	monthmap := map[string]time.Month{
+		"JAN": time.January, "FEB": time.February, "MAR": time.March,
+		"APR": time.April, "MAY": time.May, "JUN": time.June,
+		"JUL": time.July, "AUG": time.August, "SEP": time.September,
+		"OCT": time.October, "NOV": time.November, "DEC": time.December,
+	}
+
+	for i := 0; i < argc; i++ {
+		if call.Argument(i).IsNumber() {
+			n, err := call.Argument(i).ToInteger()
+			if err != nil {
+				return otto.UndefinedValue()
+			} else if 1 <= n && n <= 31 {
+				days = append(days, int(n))
+			} else {
+				years = append(years, int(n))
+			}
+		} else if month, ok := monthmap[call.Argument(i).String()]; ok {
+			months = append(months, month)
+		} else {
+			return otto.UndefinedValue()
+		}
+	}
+
+	switch max(len(days), len(months), len(years)) {
+	case 1:
+		// One (possibly partial) date provided; match it against the current date.
+		if len(days) == 1 && days[0] != now.Day() {
+			return otto.FalseValue()
+		} else if len(months) == 1 && months[0] != now.Month() {
+			return otto.FalseValue()
+		} else if len(years) == 1 && years[0] != now.Year() {
+			return otto.FalseValue()
+		} else {
+			return otto.TrueValue()
+		}
+	case 2:
+		// Two dates provided; check that the current date is inside the range.
+		y1, m1, d1 := now.Date()
+		y2, m2, d2 := now.Date()
+		if len(days) == 2 {
+			d1, d2 = days[0], days[1]
+		}
+		if len(months) == 2 {
+			m1, m2 = months[0], months[1]
+		}
+		if len(years) == 2 {
+			y1, y2 = years[0], years[1]
+		}
+		h, m, s := now.Clock()
+		ns, loc := now.Nanosecond(), now.Location()
+		start := time.Date(y1, m1, d1, h, m, s, ns, loc)
+		end := time.Date(y2, m2, d2, h, m, s, ns, loc)
+		return toValue(!start.After(now) && !end.Before(now))
+	default:
+		// Zero, three or more dates provided. Something's wrong.
+		return otto.UndefinedValue()
+	}
+}
+
+func max(a, b, c int) int {
+	if a >= b && a >= c {
+		return a
+	} else if b >= c {
+		return b
+	} else {
+		return c
+	}
+}
+
+func timeRange(call otto.FunctionCall, now time.Time) otto.Value {
+	argc := len(call.ArgumentList)
+	if call.Argument(argc-1).String() == "GMT" {
+		now = now.In(time.UTC)
+		argc--
+	}
+	h1, m1, s1, h2, m2, s2 := 0, 0, 0, 0, 0, 0
+	var err error
+	toInt := func(idx int) int {
+		val, err2 := call.Argument(idx).ToInteger()
+		if err2 != nil {
+			err = err2
+		}
+		return int(val)
+	}
+	switch argc {
+	case 1:
+		h1 = toInt(0)
+		h2 = h1 + 1
+	case 2:
+		h1 = toInt(0)
+		h2 = toInt(1)
+	case 4:
+		h1, m1 = toInt(0), toInt(1)
+		h2, m2 = toInt(2), toInt(3)
+	case 6:
+		h1, m1, s1 = toInt(0), toInt(1), toInt(2)
+		h2, m2, s2 = toInt(3), toInt(4), toInt(5)
+	default:
+		return otto.UndefinedValue()
+	}
+	if err != nil {
+		return otto.UndefinedValue()
+	}
+	start := time.Date(now.Year(), now.Month(), now.Day(), h1, m1, s1, 0, now.Location())
+	end := time.Date(now.Year(), now.Month(), now.Day(), h2, m2, s2, 0, now.Location())
+	return toValue(!start.After(now) && end.After(now))
 }
--- pacrunner_test.go
+++ pacrunner_test.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/robertkrimen/otto"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"net"
 	"net/url"
 	"strings"
 	"testing"
+	"time"
 )
 
 func newPACRunner(t *testing.T, expr string) *PACRunner {
@@ -38,25 +41,500 @@ func TestPathAndQueryStripping(t *testing.T) {
 }
 
 func TestIsPlainHostName(t *testing.T) {
-	pr := newPACRunner(t, `isPlainHostName(host) ? "y" : "n"`)
-	checkProxy(t, pr, "https://www", "y")
-	checkProxy(t, pr, "https://anz.com", "n")
+	tests := []struct {
+		host     string
+		expected bool
+	}{
+		{"www", true},
+		{"anz.com", false},
+	}
+	for _, test := range tests {
+		t.Run(test.host, func(t *testing.T) {
+			vm := otto.New()
+			require.Nil(t, vm.Set("isPlainHostName", isPlainHostName))
+			value, err := vm.Call("isPlainHostName", nil, test.host)
+			require.Nil(t, err)
+			actual, err := value.ToBoolean()
+			require.Nil(t, err)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
 }
 
 func TestDnsDomainIs(t *testing.T) {
-	pr := newPACRunner(t, `dnsDomainIs(host, ".anz.com") ? "y" : "n"`)
-	checkProxy(t, pr, "https://www.anz.com", "y")
-	checkProxy(t, pr, "https://www", "n")
+	tests := []struct {
+		host, domain string
+		expected     bool
+	}{
+		{"www.anz.com", ".anz.com", true},
+		{"www", ".anz.com", false},
+		{"notanz.com", ".anz.com", false},
+		{"notanz.com", "anz.com", true}, // https://crbug.com/299649
+	}
+	for _, test := range tests {
+		t.Run(test.host+" "+test.domain, func(t *testing.T) {
+			vm := otto.New()
+			require.Nil(t, vm.Set("dnsDomainIs", dnsDomainIs))
+			value, err := vm.Call("dnsDomainIs", nil, test.host, test.domain)
+			require.Nil(t, err)
+			actual, err := value.ToBoolean()
+			require.Nil(t, err)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
 }
 
-func TestDnsDomainIsAnySuffix(t *testing.T) {
-	// See https://bugs.chromium.org/p/chromium/issues/detail?id=299649.
-	pr := newPACRunner(t, `dnsDomainIs(host, "anz.com") ? "y" : "n"`)
-	checkProxy(t, pr, "https://notanz.com", "y")
+func TestLocalHostOrDomainIs(t *testing.T) {
+	tests := []struct {
+		name     string
+		host     string
+		hostdom  string
+		expected bool
+	}{
+		{"exact match", "www.mozilla.org", "www.mozilla.org", true},
+		{"hostname match", "www", "www.mozilla.org", true},
+		{"domain name mismatch", "www.google.com", "www.mozilla.org", false},
+		{"hostname mismatch", "home.mozilla.org", "www.mozilla.org", false},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			vm := otto.New()
+			require.Nil(t, vm.Set("localHostOrDomainIs", localHostOrDomainIs))
+			value, err := vm.Call("localHostOrDomainIs", nil, test.host, test.hostdom)
+			require.Nil(t, err)
+			actual, err := value.ToBoolean()
+			require.Nil(t, err)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
+
+func TestIsResolvable(t *testing.T) {
+	tests := []struct {
+		host     string
+		expected bool
+	}{
+		{"localhost", true},
+		{"nonexistent.test", false},
+	}
+	for _, test := range tests {
+		t.Run(test.host, func(t *testing.T) {
+			vm := otto.New()
+			require.Nil(t, vm.Set("isResolvable", isResolvable))
+			value, err := vm.Call("isResolvable", nil, test.host)
+			require.Nil(t, err)
+			actual, err := value.ToBoolean()
+			require.Nil(t, err)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
+
+func TestIsInNet(t *testing.T) {
+	tests := []struct {
+		host     string
+		pattern  string
+		mask     string
+		expected bool
+	}{
+		{"localhost", "127.0.0.0", "255.0.0.0", true},
+		{"192.0.2.1", "192.0.2.0", "255.255.255.0", true},
+		{"192.0.3.1", "192.0.2.0", "255.255.255.0", false},
+		{"192.0.3.1", "192.0.2.0", "255.255.0.0", true},
+	}
+	for _, test := range tests {
+		t.Run(test.host, func(t *testing.T) {
+			vm := otto.New()
+			require.Nil(t, vm.Set("isInNet", isInNet))
+			value, err := vm.Call("isInNet", nil, test.host, test.pattern, test.mask)
+			require.Nil(t, err)
+			actual, err := value.ToBoolean()
+			require.Nil(t, err)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
+
+func TestDnsResolve(t *testing.T) {
+	tests := []struct {
+		host     string
+		expected string
+	}{
+		{"localhost", "127.0.0.1"},
+		{"192.0.2.1", "192.0.2.1"},
+	}
+	for _, test := range tests {
+		t.Run(test.host, func(t *testing.T) {
+			vm := otto.New()
+			require.Nil(t, vm.Set("dnsResolve", dnsResolve))
+			value, err := vm.Call("dnsResolve", nil, test.host)
+			require.Nil(t, err)
+			actual, err := value.ToString()
+			require.Nil(t, err)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
+
+func TestConvertAddr(t *testing.T) {
+	tests := []struct {
+		ipaddr   string
+		expected int64
+	}{
+		{"104.16.41.2", 1745889538},
+		{"2001:db8::", 0},
+		{"www.anz.com", 0},
+	}
+	for _, test := range tests {
+		t.Run(test.ipaddr, func(t *testing.T) {
+			vm := otto.New()
+			require.Nil(t, vm.Set("convert_addr", convertAddr))
+			value, err := vm.Call("convert_addr", nil, test.ipaddr)
+			require.Nil(t, err)
+			actual, err := value.ToInteger()
+			require.Nil(t, err)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
+
+func TestMyIpAddress(t *testing.T) {
+	vm := otto.New()
+	require.Nil(t, vm.Set("myIpAddress", myIpAddress))
+	value, err := vm.Call("myIpAddress", nil)
+	require.Nil(t, err)
+	output, err := value.ToString()
+	require.Nil(t, err)
+	// Check it's a valid IPv4 address.
+	assert.NotNil(t, net.ParseIP(output).To4())
+	// Check that it's our IP address. Technically there's a race condition here (since both
+	// myIpAddress and this function will call net.InterfaceAddrs() separately), but this is
+	// only going to cause flakiness if the network changes during the test, which is unlikely.
+	addrs, err := net.InterfaceAddrs()
+	require.Nil(t, err)
+	for _, addr := range addrs {
+		if strings.HasPrefix(addr.String(), output) {
+			return
+		}
+	}
+	t.Fail()
+}
+
+func TestDnsDomainLevels(t *testing.T) {
+	tests := []struct {
+		host     string
+		expected int64
+	}{
+		{"www", 0},
+		{"mozilla.org", 1},
+		{"www.mozilla.org", 2},
+	}
+	for _, test := range tests {
+		t.Run(test.host, func(t *testing.T) {
+			vm := otto.New()
+			require.Nil(t, vm.Set("dnsDomainLevels", dnsDomainLevels))
+			value, err := vm.Call("dnsDomainLevels", nil, test.host)
+			require.Nil(t, err)
+			actual, err := value.ToInteger()
+			require.Nil(t, err)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
 }
 
 func TestShExpMatch(t *testing.T) {
-	pr := newPACRunner(t, `shExpMatch(url, "*/b/*") ? "y" : "n"`)
-	checkProxy(t, pr, "http://anz.com/a/b/c.html", "y")
-	checkProxy(t, pr, "http://anz.com/d/e/f.html", "n")
+	tests := []struct {
+		str, shexp string
+		expected   bool
+	}{
+		{"http://anz.com/a/b/c.html", "*/b/*", true},
+		{"http://anz.com/d/e/f.html", "*/b/*", false},
+	}
+	for _, test := range tests {
+		t.Run(test.str+" "+test.shexp, func(t *testing.T) {
+			vm := otto.New()
+			require.Nil(t, vm.Set("shExpMatch", shExpMatch))
+			value, err := vm.Call("shExpMatch", nil, test.str, test.shexp)
+			require.Nil(t, err)
+			actual, err := value.ToBoolean()
+			require.Nil(t, err)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
+
+func TestWeekdayRange(t *testing.T) {
+	tests := []struct {
+		name         string
+		args         []interface{}
+		expectations string
+	}{
+		{"M-F AEST", []interface{}{"MON", "FRI"}, "NYYYYYN"},
+		{"M-F UTC", []interface{}{"MON", "FRI", "GMT"}, "NNYYYYY"},
+		{"SAT AEST", []interface{}{"SAT"}, "NNNNNNY"},
+		{"SAT UTC", []interface{}{"SAT", "GMT"}, "YNNNNNN"},
+		{"F&M ONLY", []interface{}{"FRI", "MON"}, "NYNNNYN"},
+	}
+
+	// AEST is 10 hours ahead of UTC, so 5am in AEST is 7pm on the previous day in UTC.
+	sunday, err := time.Parse(time.UnixDate, "Sun Jun 30 05:00:00 AEST 2019")
+	require.Nil(t, err)
+	weekdays := []struct {
+		name string
+		t    time.Time
+	}{
+		{"SUN", sunday},
+		{"MON", sunday.Add(1 * 24 * time.Hour)},
+		{"TUE", sunday.Add(2 * 24 * time.Hour)},
+		{"WED", sunday.Add(3 * 24 * time.Hour)},
+		{"THU", sunday.Add(4 * 24 * time.Hour)},
+		{"FRI", sunday.Add(5 * 24 * time.Hour)},
+		{"SAT", sunday.Add(6 * 24 * time.Hour)},
+	}
+
+	for _, test := range tests {
+		for i, weekday := range weekdays {
+			t.Run(test.name+" "+weekday.name, func(t *testing.T) {
+				vm := otto.New()
+				f := func(fc otto.FunctionCall) otto.Value {
+					return weekdayRange(fc, weekday.t)
+				}
+				require.Nil(t, vm.Set("weekdayRange", f))
+				value, err := vm.Call("weekdayRange", nil, test.args...)
+				require.Nil(t, err)
+				actual, err := value.ToBoolean()
+				require.Nil(t, err)
+				expected := test.expectations[i] == 'Y'
+				assert.Equal(t, expected, actual)
+			})
+		}
+	}
+}
+
+func TestDateRange(t *testing.T) {
+	tests := []struct {
+		name         string
+		args         []interface{}
+		expectations map[string]bool
+	}{
+		{
+			"returns true on the first day of each month, local timezone",
+			[]interface{}{1},
+			map[string]bool{
+				"2019-06-30": false,
+				"2019-07-01": true,
+				"2019-07-02": false,
+				"2019-08-01": true,
+			},
+		}, {
+			// All of these dates are tested with a time of 5am in AEST, which is 10
+			// hours ahead of UTC. So 5am in AEST is 7pm on the previous day in UTC.
+			"returns true on the first day of each month, GMT timezone",
+			[]interface{}{1, "GMT"},
+			map[string]bool{
+				"2019-07-01": false,
+				"2019-07-02": true,
+				"2019-07-03": false,
+				"2019-08-02": true,
+			},
+		}, {
+			"returns true on the first half of each month",
+			[]interface{}{1, 15},
+			map[string]bool{
+				"2019-06-30": false,
+				"2019-07-01": true,
+				"2019-07-02": true,
+				"2019-07-15": true,
+				"2019-07-16": false,
+			},
+		}, {
+			"returns true on 24th of December each year",
+			[]interface{}{24, "DEC"},
+			map[string]bool{
+				"2019-12-23": false,
+				"2019-12-24": true,
+				"2019-12-26": false,
+				"2020-12-24": true,
+			},
+		}, {
+			"returns true on the first quarter of the year",
+			[]interface{}{"JAN", "MAR"},
+			map[string]bool{
+				"2018-12-31": false,
+				"2019-01-01": true,
+				"2019-01-02": true,
+				"2019-03-31": true,
+				"2019-04-01": false,
+				"2020-03-31": true,
+			},
+		}, {
+			"returns true from June 1st until August 15th, each year",
+			[]interface{}{1, "JUN", 15, "AUG"},
+			map[string]bool{
+				"2019-05-31": false,
+				"2019-06-01": true,
+				"2019-06-02": true,
+				"2019-08-15": true,
+				"2019-08-16": false,
+				"2020-08-15": true,
+			},
+		}, {
+			"returns true from June 1st, 1995, until August 15th, same year",
+			[]interface{}{1, "JUN", 1995, 15, "AUG", 1995},
+			map[string]bool{
+				"1995-05-31": false,
+				"1995-06-01": true,
+				"1995-06-02": true,
+				"1995-08-15": true,
+				"1995-08-16": false,
+				"1996-08-15": false,
+			},
+		}, {
+			"returns true from October 1995 until March 1996",
+			[]interface{}{"OCT", 1995, "MAR", 1996},
+			map[string]bool{
+				"1995-09-30": false,
+				"1995-10-01": true,
+				"1995-10-02": true,
+				"1996-01-01": true,
+				"1996-03-31": true,
+				"1996-04-01": false,
+				"1997-01-01": false,
+			},
+		}, {
+			"returns true during the entire year of 1995",
+			[]interface{}{1995},
+			map[string]bool{
+				"1994-12-31": false,
+				"1995-01-01": true,
+				"1995-04-19": true,
+				"1995-12-31": true,
+				"1996-01-01": false,
+			},
+		}, {
+			"returns true from beginning of year 1995 until the end of year 1997",
+			[]interface{}{1995, 1997},
+			map[string]bool{
+				"1994-12-31": false,
+				"1995-01-01": true,
+				"1996-04-19": true,
+				"1997-12-31": true,
+				"1998-01-01": false,
+			},
+		},
+	}
+
+	check := func(t *testing.T, args []interface{}, date string, expected bool) {
+		vm := otto.New()
+		now, err := time.Parse(time.RFC3339, date+"T05:00:00+10:00")
+		require.Nil(t, err)
+		f := func(fc otto.FunctionCall) otto.Value { return dateRange(fc, now) }
+		require.Nil(t, vm.Set("dateRange", f))
+		value, err := vm.Call("dateRange", nil, args...)
+		require.Nil(t, err)
+		actual, err := value.ToBoolean()
+		require.Nil(t, err)
+		assert.Equal(t, expected, actual)
+	}
+
+	for _, test := range tests {
+		for date, expected := range test.expectations {
+			t.Run(test.name+" "+date, func(t *testing.T) {
+				check(t, test.args, date, expected)
+			})
+		}
+	}
+}
+
+func TestTimeRange(t *testing.T) {
+	tests := []struct {
+		name         string
+		args         []interface{}
+		expectations map[string]bool
+	}{
+		{
+			"returns true from noon to 1pm",
+			[]interface{}{12},
+			map[string]bool{
+				"11:59:59": false,
+				"12:00:00": true,
+				"12:00:01": true,
+				"12:59:59": true,
+				"13:00:00": false,
+			},
+		}, {
+			"returns true from noon to 1pm",
+			[]interface{}{12, 13},
+			map[string]bool{
+				"11:59:59": false,
+				"12:00:00": true,
+				"12:00:01": true,
+				"12:59:59": true,
+				"13:00:00": false,
+			},
+		}, {
+			// Local time (AEST) is 10 hours ahead of UTC, so we expect timeRange to
+			// return true from 10pm to 11pm AEST.
+			"true from noon to 1pm, in GMT timezone",
+			[]interface{}{12, "GMT"},
+			map[string]bool{
+				"21:59:59": false,
+				"22:00:00": true,
+				"22:00:01": true,
+				"22:59:59": true,
+				"23:00:00": false,
+			},
+		}, {
+			"returns true from 9am to 5pm",
+			[]interface{}{9, 17},
+			map[string]bool{
+				"08:59:59": false,
+				"09:00:00": true,
+				"09:00:01": true,
+				"16:59:59": true,
+				"17:00:00": false,
+			},
+		}, {
+			"returns true from 8:30am to 5:00pm",
+			[]interface{}{8, 30, 17, 00},
+			map[string]bool{
+				"08:29:59": false,
+				"08:30:00": true,
+				"08:30:01": true,
+				"16:59:59": true,
+				"17:00:00": false,
+			},
+		}, {
+			"returns true between midnight and 30 seconds past midnight",
+			[]interface{}{0, 0, 0, 0, 0, 30},
+			map[string]bool{
+				"00:00:00": true,
+				"00:00:01": true,
+				"00:00:29": true,
+				"00:00:30": false,
+				"23:59:59": false,
+			},
+		},
+	}
+
+	check := func(t *testing.T, args []interface{}, mocktime string, expected bool) {
+		vm := otto.New()
+		now, err := time.Parse(time.RFC3339, "2019-07-01T"+mocktime+"+10:00")
+		require.Nil(t, err)
+		f := func(fc otto.FunctionCall) otto.Value { return timeRange(fc, now) }
+		require.Nil(t, vm.Set("timeRange", f))
+		value, err := vm.Call("timeRange", nil, args...)
+		require.Nil(t, err)
+		actual, err := value.ToBoolean()
+		require.Nil(t, err)
+		assert.Equal(t, expected, actual)
+	}
+
+	for _, test := range tests {
+		for mocktime, expected := range test.expectations {
+			t.Run(test.name+" "+mocktime, func(t *testing.T) {
+				check(t, test.args, mocktime, expected)
+			})
+		}
+	}
 }
