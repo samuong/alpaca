25fd9e122d60a7608247a7981559f808edf80510
Sam Uong <samuong@gmail.com>
2020-09-28 09:43:09 -0400

    Add godocs for github.com/samuong/alpaca/cancelable

--- cancelable/closer.go
+++ cancelable/closer.go
@@ -1,4 +1,4 @@
-// Copyright 2019 The Alpaca Authors
+// Copyright 2019, 2020 The Alpaca Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,24 +12,34 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package cancelable provides a cancelable version of io.Closer.
+//
+// This is intended to be used by functions where the responsibility for closing
+// a resource sometimes belongs to the function itself (e.g. early returns due
+// to error handling), and sometimes belongs to the caller.
 package cancelable
 
 import (
 	"io"
 )
 
+// Closer is an io.Closer that can be cancelled.
 type Closer struct {
 	c io.Closer
 }
 
+// NewCloser returns a cancelable.Closer that wraps an io.Closer.
 func NewCloser(c io.Closer) *Closer {
 	return &Closer{c: c}
 }
 
+// Cancel turns any future calls to Close into a no-op.
 func (c *Closer) Cancel() {
 	c.c = nil
 }
 
+// Close calls the Close function on the io.Closer (if it has not been cancelled
+// or already closed). It is safe to call this multiple times.
 func (c *Closer) Close() error {
 	if c.c == nil {
 		return nil
