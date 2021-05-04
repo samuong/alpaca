933e90b333bb41de5cf9dc4dbe31f1e1bd023780
Sam Uong <samuong@gmail.com>
2021-05-04 22:15:39 +1000

    Split pacfinder(_test).go into versions for darwin/unix/windows
    The tests currently fail on Windows due to the darwin and unix code not
    working on Windows. This change fixes that.

---
+++ pacfinder_unix.go
@@ -0,0 +1,33 @@
+// Copyright 2019, 2021 The Alpaca Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build aix dragonfly freebsd linux netbsd openbsd solaris
+
+package main
+
+import (
+	"os/exec"
+	"strings"
+)
+
+func findPACURL() (string, error) {
+	// Hopefully Linux, FreeBSD, Solaris, etc. will have GNOME 3 installed...
+	// TODO: Figure out how to do this for KDE.
+	cmd := exec.Command("gsettings", "get", "org.gnome.system.proxy", "autoconfig-url")
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return strings.Trim(string(out), "'\n"), nil
+}
---
+++ pacfinder_unix_test.go
@@ -0,0 +1,56 @@
+// Copyright 2019, 2021 The Alpaca Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build aix dragonfly freebsd linux netbsd openbsd solaris
+
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+
+func TestFindPACURL(t *testing.T) {
+	dir, err := ioutil.TempDir("", "alpaca")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+	oldpath := os.Getenv("PATH")
+	defer require.NoError(t, os.Setenv("PATH", oldpath))
+
+	require.NoError(t, os.Setenv("PATH", dir))
+	tmpfn := filepath.Join(dir, "gsettings")
+	mockcmd := "#!/bin/sh\necho \\'http://internal.anz.com/proxy.pac\\'\n"
+	require.NoError(t, ioutil.WriteFile(tmpfn, []byte(mockcmd), 0700))
+
+	pacURL, err := findPACURL()
+	require.NoError(t, err)
+	assert.Equal(t, "http://internal.anz.com/proxy.pac", pacURL)
+}
+
+func TestFindPACURLWhenGsettingsIsntAvailable(t *testing.T) {
+	dir, err := ioutil.TempDir("", "alpaca")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+	oldpath := os.Getenv("PATH")
+	defer require.NoError(t, os.Setenv("PATH", oldpath))
+	require.NoError(t, os.Setenv("PATH", dir))
+	_, err = findPACURL()
+	require.NotNil(t, err)
+}
