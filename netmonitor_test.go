31fb72c72de07e32be0a800458bad54d3e43b68a
Sam Uong <samuong@gmail.com>
2024-07-21 17:32:08 +1000

    Check routes when checking for network changes

--- netmonitor_test.go
+++ netmonitor_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 The Alpaca Authors
+// Copyright 2019, 2024 The Alpaca Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,10 +16,13 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"net"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 type mockAddr string
@@ -40,28 +43,137 @@ func toAddrs(ss ...string) []net.Addr {
 	return addrs
 }
 
+type mockConn struct {
+	localAddr net.Addr
+}
+
+var _ net.Conn = mockConn{}
+
+func (c mockConn) Close() error {
+	return nil
+}
+
+func (c mockConn) LocalAddr() net.Addr {
+	return c.localAddr
+}
+
+func (c mockConn) Read(b []byte) (n int, err error) {
+	panic("unreachable")
+}
+
+func (c mockConn) RemoteAddr() net.Addr {
+	panic("unreachable")
+}
+
+func (c mockConn) SetDeadline(t time.Time) error {
+	panic("unreachable")
+}
+
+func (c mockConn) SetReadDeadline(t time.Time) error {
+	panic("unreachable")
+}
+
+func (c mockConn) SetWriteDeadline(t time.Time) error {
+	panic("unreachable")
+}
+
+func (c mockConn) Write(b []byte) (n int, err error) {
+	panic("unreachable")
+}
+
+type mockNet struct {
+	connectedTo string
+}
+
+func (n *mockNet) interfaceAddrs() ([]net.Addr, error) {
+	var addrs []net.Addr
+	switch n.connectedTo {
+	case "vpn", "wifi":
+		addrs = append(addrs, toAddrs("192.168.1.2/24", "fe80::fedc:ba98:7654:3210/64")...)
+		fallthrough
+	case "offline":
+		addrs = append(addrs, toAddrs("127.0.0.1/8", "::1/128")...)
+	default:
+		panic("interfaceAddrs connectedTo=" + n.connectedTo)
+	}
+	return addrs, nil
+}
+
+func (n *mockNet) dial(network, address string) (net.Conn, error) {
+	if network != "udp" && network != "udp4" {
+		panic("dial network=" + network)
+	}
+	host, _, err := net.SplitHostPort(address)
+	if err != nil {
+		panic("dial: " + err.Error())
+	}
+	ip := net.ParseIP(host)
+	if ip == nil {
+		panic("dial host=" + host)
+	}
+	if ipv4 := ip.To4(); ipv4 == nil {
+		return nil, &net.OpError{
+			Op:     "dial",
+			Net:    network,
+			Source: nil,
+			Addr:   mockAddr(address),
+			Err:    errors.New("connect: no route to host"),
+		}
+	}
+	switch n.connectedTo {
+	case "vpn":
+		return mockConn{localAddr: &net.UDPAddr{IP: net.IPv4(10, 0, 0, 3)}}, nil
+	case "wifi":
+		return mockConn{localAddr: &net.UDPAddr{IP: net.IPv4(192, 168, 1, 2)}}, nil
+	case "offline":
+		return nil, &net.OpError{
+			Op:     "dial",
+			Net:    network,
+			Source: nil,
+			Addr:   mockAddr(address),
+			Err:    errors.New("connect: network is unreachable"),
+		}
+	default:
+		panic("dial connectedTo=" + n.connectedTo)
+	}
+}
+
 func TestNetworkMonitor(t *testing.T) {
-	var next []net.Addr
-	nm := &netMonitorImpl{getAddrs: func() ([]net.Addr, error) { return next, nil }}
-	// Start with just loopback interfaces
-	next = toAddrs("127.0.0.1/8", "::1/128")
+	var network mockNet
+	nm := &netMonitorImpl{getAddrs: network.interfaceAddrs, dial: network.dial}
+	network.connectedTo = "offline"
 	assert.True(t, nm.addrsChanged())
-	// Connect to network, and get local IPv4 and IPv6 addresses
-	next = toAddrs("127.0.0.1/8", "192.168.1.6/24", "::1/128", "fe80::dfd9:fe1d:56d1:1f3a/64")
+	network.connectedTo = "wifi"
 	assert.True(t, nm.addrsChanged())
-	// Stay connected, nothing changed
-	next = toAddrs("127.0.0.1/8", "192.168.1.6/24", "::1/128", "fe80::dfd9:fe1d:56d1:1f3a/64")
 	assert.False(t, nm.addrsChanged())
-	// DHCP lease expires, get new addresses
-	next = toAddrs("127.0.0.1/8", "192.168.1.7/24", "::1/128", "fe80::dfd9:fe1d:56d1:1f3b/64")
+	network.connectedTo = "vpn"
+	fmt.Println(`network.connectedTo = "vpn"`)
 	assert.True(t, nm.addrsChanged())
-	// Disconnect, and go back to having just loopback addresses
-	next = toAddrs("127.0.0.1/8", "::1/128")
+	fmt.Println(`network.connectedTo = "offline"`)
+	network.connectedTo = "offline"
 	assert.True(t, nm.addrsChanged())
 }
 
-func TestFailToGetAddrs(t *testing.T) {
-	alwaysFail := func() ([]net.Addr, error) { return nil, errors.New("failed") }
-	nm := &netMonitorImpl{getAddrs: alwaysFail}
-	assert.False(t, nm.addrsChanged())
+func TestDumpAddrs(t *testing.T) {
+	ifaces, err := net.Interfaces()
+	require.NoError(t, err)
+	t.Log("---------- INTERFACES AND ADDRESSES: ----------")
+	for _, iface := range ifaces {
+		addrs, err := iface.Addrs()
+		require.NoError(t, err)
+		t.Logf("%s -> %q", iface.Name, addrs)
+	}
+	remotes := []string{
+		"8.8.8.8", "2001:4860:4860::8888", // public addresses
+		"10.0.0.0", "172.16.0.0", "192.168.0.0", "FC00::", // private addresses
+	}
+	t.Log("---------- ROUTES: ----------")
+	for _, addr := range remotes {
+		conn, err := net.Dial("udp", net.JoinHostPort(addr, "80"))
+		if err != nil {
+			t.Logf("%q => error: %v\n", addr, err)
+			continue
+		}
+		t.Logf("%q => %q\n", addr, conn.LocalAddr().String())
+	}
 }
