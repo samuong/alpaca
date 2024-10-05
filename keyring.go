8a18678cbbd1c2a9d48cef6ba7be8c80996d8705
giuliano <panzironi.giuliano@gmail.com>
2024-10-05 16:39:47 +0200

    Let the keyring.ErrUnsupportedPlatform err be returned if keyring impl is missing for platforms other than Linux and Windows

--- keyring.go
+++ keyring.go
@@ -1,4 +1,4 @@
-// Copyright 2019, 2020, 2021 The Alpaca Authors
+// Copyright 2024 The Alpaca Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build linux || windows
-// +build linux windows
+//go:build !darwin
 
 package main
 
